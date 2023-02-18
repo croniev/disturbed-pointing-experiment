47221a0ab9707f4a21bcaaa91db04f616263afa8
levin <levin.niebler@stud.tu-darmstadt.de>
2023-02-18 15:15:59 +0100

    fixed distortions, added back-task and question

--- main.py
+++ main.py
@@ -43,6 +43,7 @@ from psychopy import core, event, visual
 def main(
     screen: int = 0,
     debug: bool = False,
+    distortion: str = "random"
 ) -> None:
     # Open a PsychoPy window, show the image, collect a keypress, close.
     win = visual.Window(
@@ -51,17 +52,18 @@ def main(
         units="pix",
         allowGUI=True,
         fullscr=not(debug), # FINAL: debug auf False setzen
-        blendMode="add"
     )
 
     hit_radius = 20 # TODO: hit_radius als parameter
-    n_trials = 3 # TODO: als parameter
+    n_trials = 20 # TODO: als parameter
+    spawn = [0,-300] # TODO: variierbar
+    target_options = [230,270,320]
 
     instruction_text = visual.TextStim(
         win,
         units="norm",
         text=(
-            "Press space to continue."
+            "During the experiment, press Q to quit. \nPress space to continue."
         ),
         wrapWidth=0.9,
         height=0.05,
@@ -70,7 +72,12 @@ def main(
     instruction_text.draw()
     win.setMouseVisible(False)
     win.flip()
-    event.waitKeys(keyList=["space"])
+    pressed_key = event.waitKeys(keyList=["space","q"])
+
+    if pressed_key[0][0] == "q":
+        win.setMouseVisible(True)
+        win.close()
+        core.quit()
 
     mouse = Mouse(visible=False, win = win)
     mouse.clickReset()
@@ -81,9 +88,17 @@ def main(
     for trial in range(n_trials):
         # Setup trial
         completed = False
-        mouse.setPos([0,-200])
-        target_pos = [0,random.choice([160,190,220])]
-        dist_type = "repell" #random.choice([None,"straight","rotate","repell","burst"],p=[0.8,0.05,0.05,0.05,0.05])
+        mouse.setPos(spawn)
+        target_pos = [0,random.choice(target_options)]
+        if distortion == "random":
+            dist_type = np.random.choice(["none","straight","rotate","repell","burst"],p=[0.8,0.05,0.05,0.05,0.05])
+        else:
+            dist_type = distortion
+        # Setup burst distort
+        burst_time = 0.5
+        burst_dur = 0.2
+        burst_force = -10 # Horizontal burst force
+        timer = core.Clock()
 
         target_circle = new_circle(win,target_pos,r=20,color=(200,0,50))
 
@@ -91,19 +106,25 @@ def main(
             mouse_pos = mouse.getPos()
 
             # Distortions
-            if (dist_type == None):
+            if (dist_type == "none"):
                 new_mouse_pos = (mouse_pos[0],mouse_pos[1])
             if (dist_type == "straight"):
                 new_mouse_pos = (0,mouse_pos[1])
             if (dist_type == "rotate"):
-                new_mouse_pos = rotate_point(mouse_pos, 30, origin=(0,-200))
+                new_mouse_pos = rotate_point(mouse_pos, 30, origin=(0,-200)) # TODO: make parameter
             if (dist_type == "repell"):
-                new_mouse_pos = repell(mouse_pos, target_pos, thresh=160, force=0.01)
+                new_mouse_pos = repell(mouse_pos, target_pos, thresh=120, force=0.03) # TODO: make parameter
                 mouse.setPos((new_mouse_pos[0],new_mouse_pos[1]+1)) # +1 because of bug with setPos()
+            if (dist_type == "burst"):
+                if timer.getTime() >= burst_time and timer.getTime() <= burst_time+burst_dur:
+                    new_mouse_pos = [mouse_pos[0]+burst_force,mouse_pos[1]]
+                    mouse.setPos((new_mouse_pos[0],new_mouse_pos[1]+1)) # +1 because of bug with setPos()
+                else: 
+                    new_mouse_pos = mouse_pos
 
             # Draw Stuff
-            new_circle(win,new_mouse_pos).draw()
             target_circle.draw()
+            new_circle(win,new_mouse_pos).draw()
             win.flip()
 
             # Breakout condition
@@ -112,6 +133,34 @@ def main(
                 completed = True
             core.wait(0.005)
 
+        # Move mouse back
+        back = False
+        mouse.setPos(target_pos)
+        while not back:
+            new_circle(win,mouse.getPos()).draw() 
+            new_circle(win,spawn, color=(200,200,255)).draw() # TODO: Use cross instead
+            win.flip()
+
+            if math.dist(mouse.getPos(), spawn) < hit_radius:
+                back = True
+            core.wait(0.005)
+
+        # Ask question
+        feedback_text = visual.TextStim(
+                win,
+                units="norm",
+                text=("Press Y if you felt a distortion in your arm and N if you did not. \nPress Q to quit"),
+                wrapWidth=0.9,
+                height=0.05
+        )
+        feedback_text.draw()
+        win.flip()
+
+        pressed_key = event.waitKeys(keyList=["q","y","n"])
+        if pressed_key[0][0] == "q":
+            win.setMouseVisible(True)
+            win.close()
+            core.quit()
 
     # END SESSION
     win.setMouseVisible(True)
@@ -138,8 +187,8 @@ def rotate_point(point, deg, origin=(0,0)):
 
 def repell(point, target, thresh, force):
     dist = math.dist(point, target)
-    newx = point[0]+np.sign(point[0]-target[0])*force*max(0,thresh-dist)
-    newy = point[1]+np.sign(point[1]-target[1])*force*max(0,thresh-dist)
+    newx = point[0]+np.sign(point[0]-target[0])*math.ceil(force*max(0,thresh-dist))
+    newy = point[1]+np.sign(point[1]-target[1])*math.ceil(force*max(0,thresh-dist))
     return (newx,newy)
 
 @click.command()
@@ -158,10 +207,21 @@ def repell(point, target, thresh, force):
     show_default=True,
     help="Pass to enable debug mode (disable fullscreen, increase logging level)",
 )
-def cli(screen: int, debug: bool):
+
+@click.option(
+        "-d",
+        "--distortion",
+        default="random",
+        type=str,
+        show_default=True,
+        help="set distortion to one of [random,none,straight,rotate,repell,burst].",
+)
+
+def cli(screen: int, debug: bool, distortion: str):
     main(
         screen=screen,
         debug=debug,
+        distortion=distortion,
     )
 
 
