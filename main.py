cb595d9caa8499f595b51971f551bba9c6946e68
levin <levin.niebler@stud.tu-darmstadt.de>
2023-03-11 14:02:03 +0100

    focus on burst, blocks, training

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-"""
-Load an image, save to JPEG, show with PsychoPy and
-collect a key response.
-
-Run from the command line, from the /src directory. Example:
-
-python basic_demo.py -d "jpg" -p 50
-python basic_demo.py -d "blur" -p 2
-
-For more options type
-
-python basic_demo.py --help
-"""
-
 from pathlib import Path
 import os
 from PIL.Image import blend
@@ -21,24 +7,40 @@ import random
 from datetime import datetime
 import sys
 import click
+import warnings
 from psychopy import prefs, monitors
 from psychopy.visual.window import Window
 from psychopy.event import Mouse
 from psychopy import core
-
 from psychopy.tools import monitorunittools
 import math
-#from pyrsistent import T
+from psychopy import core, event, visual
+import bisect
 
 # to avoid bug on windows (https://www.psychopy.org/troubleshooting.html#errors-with-getting-setting-the-gamma-ramp)
 prefs.general["gammaErrorPolicy"] = "warn"
-
 size = 250
 
-from psychopy import core, event, visual
-#from gen_pink_noise import pink_noise_image
-#from scipy import fftpack
-# https://www.psychopy.org/general/rendering.html#blendmode
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# PARAMETERS
+touch_radius = 25 # How close one has to be to beginning or orientation point
+n_training = 1 # How many training blocks are performed
+n_trials = 3 # How many normal blocks are performed
+block_size = 30 # How many trials are in on block
+orientation_point = [0,-350] # Position of point that has to be cut
+beginning_point = [0,-470] # Position of point that needs to be gone back to
+target_pos = (0,300) # not used
+top_pos  =(0,350) # Where mouse will spawn after task and reference point for scoring
+time_score_dist_crit = 60 # How far the mouse has to have been away from top to be penalized
+time_score_time_crit = 7 # How many steps too fast mouse has to have been to be penalized
+refresh_rate=0.0001 # After how much time mouse position is updated
+disturb_prob = 0.2 # How likely a disturbtion is to occur in a normal trial
+burst_time = 0.17 # After how much time the burst disturbtion starts
+burst_dur = 0.2 # How long the burst disturbtion is applied
+burst_force = -20 # Horizontal force of the burst disturbtion
+time_limit = 0.4 # How much time one has for a trial
+question_prob = 0.4 # Likelihood of subjective reporting in a non-disturbtion trial
 
 def main(
     screen: int = 0,
@@ -54,118 +56,226 @@ def main(
         fullscr=not(debug), # FINAL: debug auf False setzen
     )
 
-    hit_radius = 20 # TODO: hit_radius als parameter
-    n_trials = 20 # TODO: als parameter
-    spawn = [0,-300] # TODO: variierbar
-    target_options = [230,270,320]
+    # START EXPERIMENT
+    mouse = Mouse(visible=False, win = win)
+    start_experiment(win, mouse)
+    core.wait(0.3)
+    score = None
+    timing = None
 
+    # DATA COLLECTION
+    columns = ['trial_nr','is_training','block_size','block_nr'] # TODO: What data should be saved
+    df = pd.DataFrame(columns=columns) 
+    df["is_training"]=df["is_training"].astype(bool)
+    trial_nr = 0
+    block_nr = 1
+    
+    training = True
+    while (True):
+        # Pause
+        if trial_nr % block_size == 0 and trial_nr != 0:
+            block_nr += 1
+            if (block_nr > n_training+n_trials):
+                break
+
+            if (block_nr >= n_training and training):
+                training = False
+                visual.TextStim(win,
+                        units="norm",
+                        text=("You have completed the training block.\nPress 'space' to start with the experiment."),
+                        wrapWidth=0.9,
+                        height=0.05
+                ).draw()
+            else:
+                visual.TextStim(win,
+                        units="norm",
+                        text=("You have completed a block.\nRelax for a bit.\nPress 'space' when you are ready to continue."),
+                        wrapWidth=0.9,
+                        height=0.05
+                ).draw()
+
+            win.flip()
+            if event.waitKeys(keyList=["space","q"])[0] == "q":
+                win.setMouseVisible(True)
+                win.close()
+                core.quit()
+
+        trial_nr += 1
+        trial_data = {"trial_nr":[trial_nr],"is_training":[training],"block_size":[block_size],"block_nr":[block_nr]}
+        back = False
+        if len(event.getKeys(keyList=['q'])) > 0:
+            win.setMouseVisible(True)
+            win.close()
+            core.quit()
+
+        # Preparation - move mouse back
+        started = False
+        mouse.setPos(top_pos)
+        while not back:
+            if len(event.getKeys(keyList=['q'])) > 0: # DEBUG
+                win.setMouseVisible(True)
+                win.close()
+                core.quit()
+
+            # Show score
+            visual.TextStim(win,
+                    units="norm",
+                    text=(str(timing)+"\nScore: "+str(score)),
+                    wrapWidth=0.9,
+                    height=0.035,
+                    alignText="left"
+            ).draw()
+
+            if training:
+                visual.Line(win,
+                        start=(0,-1000),
+                        end=(0,1000),
+                        lineWidth=5,
+                        color=(0,0,0),
+                        colorSpace='rgb255'
+                        ).draw()
+            
+            if not(started):
+                new_circle(win, beginning_point, r=touch_radius, color=(200,200,255)).draw()
+                new_circle(win,mouse.getPos(), color=(0,200,5)).draw() 
+                if math.dist(mouse.getPos(),beginning_point) < touch_radius:
+                    started = True
+            else:
+                new_circle(win,orientation_point, r=7, color=(200,200,255)).draw() 
+                new_circle(win,mouse.getPos()).draw() 
+                if math.dist(mouse.getPos(), orientation_point) < touch_radius:
+                    back = True
+            win.flip()
+            core.wait(refresh_rate)
+
+        # Pick distortion
+#        if distortion == "random":
+#            dist_type = np.random.choice(["none","straight","rotate","repell","burst"],p=[0.8,0.05,0.05,0.05,0.05])
+#        else:
+#            dist_type = distortion
+        if (training):
+            dist_type = "none"
+        else:
+            dist_type = np.random.choice(["none","burst"],p=[1-disturb_prob,disturb_prob])
+
+        timer = core.Clock()
+        offset = 0
+
+        # Task
+        completed = False
+        y_array = []
+        err=0
+        while(not completed):
+            if len(event.getKeys(keyList=['q'])) > 0: # DEBUG
+                win.setMouseVisible(True)
+                win.close()
+                core.quit()
+            mouse_pos = mouse.getPos()
+
+#            # Distortions
+#            if (dist_type == "none"):
+#                virtual_mouse_pos = (mouse_pos[0],mouse_pos[1])
+#            if (dist_type == "straight"):
+#                virtual_mouse_pos = (0,mouse_pos[1])
+#            if (dist_type == "rotate"):
+#                virtual_mouse_pos = rotate_point(mouse_pos, 30, origin=(0,-200)) 
+#            if (dist_type == "repell"):
+#                virtual_mouse_pos = repell(mouse_pos, target_pos, thresh=120, force=0.03) 
+#                mouse.setPos((virtual_mouse_pos[0],virtual_mouse_pos[1]+1)) # +1 because of bug with setPos()
+#            if (dist_type == "burst"):
+#                if timer.getTime() >= burst_time and timer.getTime() <= burst_time+burst_dur:
+#                    virtual_mouse_pos = [mouse_pos[0]+burst_force,mouse_pos[1]]
+#                    mouse.setPos((virtual_mouse_pos[0],virtual_mouse_pos[1]+1)) # +1 because of bug with setPos()
+#                else: 
+#                    virtual_mouse_pos = mouse_pos
+
+            # Apply Distortion
+            if (dist_type == "burst" and timer.getTime() >= burst_time and timer.getTime() <= burst_time+burst_dur):
+                offset += burst_force
+            virtual_mouse_pos = [mouse_pos[0]+offset,mouse_pos[1]]
+
+            # Draw Stuff
+            if training:
+                visual.Line(win,
+                        start=(0,-1000),
+                        end=(0,1000),
+                        lineWidth=5,
+                        color=(0,0,0),
+                        colorSpace='rgb255'
+                        ).draw()
+            new_circle(win,orientation_point, r=7, color=(100,255,100)).draw() 
+            new_circle(win,virtual_mouse_pos).draw()
+            win.flip()
+
+            # Scoring
+            y_array.append(mouse_pos[1])
+            err += np.absolute(0-mouse_pos[0]) # TODO: use mouse pos or virtual mouse pos?
+
+            # Breakout condition
+            if timer.getTime() >= time_limit:
+                completed = True
+            core.wait(refresh_rate)
+
+        score, timing = scoring(y_array, err)
+        # Ask question
+        if not(training) and (dist_type=="burst" or np.random.choice([True,False],p=[question_prob,1-question_prob])):
+            feedback_text = visual.TextStim(
+                    win,
+                    units="norm",
+                    text=("Press Y if you felt a distortion in your arm and N if you did not."),
+                    wrapWidth=0.9,
+                    height=0.05
+            )
+            feedback_text.draw()
+            win.flip()
+            pressed_key = event.waitKeys(keyList=["q","y","n"])
+            if pressed_key[0][0] == "q": # DEBUG
+                win.setMouseVisible(True)
+                win.close()
+                core.quit()
+        else:
+            win.flip()
+            core.wait(0.2)
+
+        # Add data to df
+        tmp = pd.DataFrame.from_dict(trial_data)
+        df = pd.concat([df,tmp],ignore_index=True)
+
+    # END SESSION
+    feedback_text = visual.TextStim(
+            win,
+            units="norm",
+            text=("You have completed the experiment!\nThank you very much for your participation, have a nice day."),
+            wrapWidth=0.9,
+            height=0.05
+    ).draw()
+    win.flip()
+    event.waitKeys()
+    win.setMouseVisible(True)
+    win.close()
+    df.to_csv('data.csv', index=False) # TODO: different file names for participants
+    core.quit()
+
+def start_experiment(win,mouse):
     instruction_text = visual.TextStim(
         win,
         units="norm",
         text=(
-            "During the experiment, press Q to quit. \nPress space to continue."
+            "Your task is to move your cursor up in a straight line. First you will complete some training trials, and then the experiment will beginn.\nWhen the cursor is green, please move it back to the orientation_point point. It will then turn white, and you should again move it in a straight line.\n\nDuring the experiment, press 'q' to quit. \nPress space to continue."
         ),
         wrapWidth=0.9,
         height=0.05,
     )
-   
     instruction_text.draw()
     win.setMouseVisible(False)
     win.flip()
     pressed_key = event.waitKeys(keyList=["space","q"])
-
-    if pressed_key[0][0] == "q":
+    if pressed_key[0][0] == "q": #DEBUG
         win.setMouseVisible(True)
         win.close()
         core.quit()
-
-    mouse = Mouse(visible=False, win = win)
     mouse.clickReset()
-
     win.flip()
-    core.wait(0.3)
-    
-    for trial in range(n_trials):
-        # Setup trial
-        completed = False
-        mouse.setPos(spawn)
-        target_pos = [0,random.choice(target_options)]
-        if distortion == "random":
-            dist_type = np.random.choice(["none","straight","rotate","repell","burst"],p=[0.8,0.05,0.05,0.05,0.05])
-        else:
-            dist_type = distortion
-        # Setup burst distort
-        burst_time = 0.5
-        burst_dur = 0.2
-        burst_force = -10 # Horizontal burst force
-        timer = core.Clock()
-
-        target_circle = new_circle(win,target_pos,r=20,color=(200,0,50))
-
-        while(not completed):
-            mouse_pos = mouse.getPos()
-
-            # Distortions
-            if (dist_type == "none"):
-                new_mouse_pos = (mouse_pos[0],mouse_pos[1])
-            if (dist_type == "straight"):
-                new_mouse_pos = (0,mouse_pos[1])
-            if (dist_type == "rotate"):
-                new_mouse_pos = rotate_point(mouse_pos, 30, origin=(0,-200)) # TODO: make parameter
-            if (dist_type == "repell"):
-                new_mouse_pos = repell(mouse_pos, target_pos, thresh=120, force=0.03) # TODO: make parameter
-                mouse.setPos((new_mouse_pos[0],new_mouse_pos[1]+1)) # +1 because of bug with setPos()
-            if (dist_type == "burst"):
-                if timer.getTime() >= burst_time and timer.getTime() <= burst_time+burst_dur:
-                    new_mouse_pos = [mouse_pos[0]+burst_force,mouse_pos[1]]
-                    mouse.setPos((new_mouse_pos[0],new_mouse_pos[1]+1)) # +1 because of bug with setPos()
-                else: 
-                    new_mouse_pos = mouse_pos
-
-            # Draw Stuff
-            target_circle.draw()
-            new_circle(win,new_mouse_pos).draw()
-            win.flip()
-
-            # Breakout condition
-            dist = math.dist(new_mouse_pos, target_pos)
-            if dist < hit_radius:
-                completed = True
-            core.wait(0.005)
-
-        # Move mouse back
-        back = False
-        mouse.setPos(target_pos)
-        while not back:
-            new_circle(win,mouse.getPos()).draw() 
-            new_circle(win,spawn, color=(200,200,255)).draw() # TODO: Use cross instead
-            win.flip()
-
-            if math.dist(mouse.getPos(), spawn) < hit_radius:
-                back = True
-            core.wait(0.005)
-
-        # Ask question
-        feedback_text = visual.TextStim(
-                win,
-                units="norm",
-                text=("Press Y if you felt a distortion in your arm and N if you did not. \nPress Q to quit"),
-                wrapWidth=0.9,
-                height=0.05
-        )
-        feedback_text.draw()
-        win.flip()
-
-        pressed_key = event.waitKeys(keyList=["q","y","n"])
-        if pressed_key[0][0] == "q":
-            win.setMouseVisible(True)
-            win.close()
-            core.quit()
-        core.wait(0.2)
-    # END SESSION
-    win.setMouseVisible(True)
-    win.close()
-    core.quit()
 
 def new_circle(win, pos: tuple, r=10,color=(255,255,255)):
     new_circ=visual.Circle(
@@ -176,6 +286,27 @@ def new_circle(win, pos: tuple, r=10,color=(255,255,255)):
             fillColor = color,
             colorSpace = 'rgb255')
     return new_circ
+
+def scoring(y_array, err):
+    score = 1000
+
+    # score for time
+    timing = "Good Timing"
+    reach_top_idx = bisect.bisect_left(y_array, top_pos[1])
+    print(y_array)
+    print(reach_top_idx)
+    if reach_top_idx == len(y_array): # i.e. top was not reached
+        min_dist = np.absolute(top_pos[1]-max(y_array))
+        if(min_dist > time_score_dist_crit):
+            timing = "Too slow (too short)!"
+            score -= min_dist # TODO: how this?
+    elif reach_top_idx <= time_score_time_crit:
+        timing = "Too fast!"
+        score -= (reach_top_idx)/10 # TODO: how this?
+
+    # score for straight
+    score -= err # TODO: how this?
+    return int(np.round(score)), timing
 
 def rotate_point(point, deg, origin=(0,0)):
     angle = np.deg2rad(deg)
