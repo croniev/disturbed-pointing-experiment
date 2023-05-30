258c34df6df09b8e0641600201531419d196d135
croniev <croniev.git@runbox.com>
2023-05-30 09:46:08 +0200

    Analysis structure

--- analysis.py
+++ analysis.py
@@ -1,3 +1,5 @@
+import sys
+import inspect
 import os
 import numpy as np
 import pandas as pd
@@ -8,17 +10,31 @@ import seaborn as sb
 
 
 def main(
+        args: list
 ) -> None:
-    print("servus")
-    # Hier die entsprechende Methode aufrufen
+    if (len(args) == 0):
+        print("No method to be executed was passed. Please ececute `analysis.py <args>`.")
+    print("Available methods are:", [name for name, obj in inspect.getmembers(sys.modules[__name__], inspect.isfunction) if name != "main" and name != "cli"])
+
+    for arg in args:  # Die übergebenen Methoden ausführen
+        print("Executing method", arg)
+        globals()[arg]()
+
+        # show plot
+
+        # save plot
+
+    # Exit
 
 
 # Methoden für analyse schreiben (inkl. plt.savefig())
+def test():
+    print("test successfull")
 
 
 def cli():
     main(
-    )
+        args=sys.argv[1:])
 
 
 if __name__ == "__main__":
